97c2c0ed1e6142aabb41576ae4189d52d455d1c1
asdfash <jit.tham@gmail.com>
2022-10-01 23:57:13 -0400

    done

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ from bisect import bisect_left
 import random
 
 class player():
-    def __init__(self,hp:int,armour:int,energy:int,handsize:int,cards:list,buffs:list,deck:list,discard:list):
+    def __init__(self,hp:int,armour:int,energy:int,handsize:int,cards:list,buffs:list,boost:list,deck:list,discard:list):
         self.hp = hp
         self.maxhp = hp
         self.armour = armour
@@ -13,21 +13,30 @@ class player():
         self.cards = cards
         self.deck = deck
         self.discard = discard
-        random.shuffle(self.deck)
-        self.newturn()
         self.prevhp = hp
+        self.boost = boost # [hp1 add,hp1 mul, hp2 add, hp2 mul] 
+        self.bdm = 1
+        self.selflove = 0
+        self.temp = 0
+        self.htay = 0
+        
+        
 
     def draw(self):
-        if len(self.deck) > 0:
-            self.cards.append(self.deck.pop())
-        elif len(self.discard) > 0:
-            self.deck = self.discard[::]
-            self.discard = []
-            random.shuffle(self.deck)
-            self.cards.append(self.deck.pop())
+        if len(self.cards)<9:
+            if len(self.deck) > 0:
+                self.cards.append(self.deck.pop())
+
+            elif len(self.discard) > 0:
+                self.deck = self.discard[::]
+                self.discard = []
+                random.shuffle(self.deck)
+                self.cards.append(self.deck.pop())
+            else:
+                raise("tried to draw, ran out of cards!")
         else:
-            raise("tried to draw, ran out of cards!")
-    
+            return
+
     def play(self,istr:str):
         if istr == "end turn":
             return 2
@@ -38,6 +47,8 @@ class player():
                 self.cards.remove(card)
                 self.discard.append(card)
                 self.energy -= card.activate()
+                if self.htay > 0:
+                    self.hp -= self.htay
                 if self.energy <= 0 or len(self.cards)<=0:
                     return 2
                 return 1
@@ -45,13 +56,17 @@ class player():
         return 0
 
     def newturn(self,extraenergy=0):
-        self.armour = 0
-        while self.handsize>len(self.cards) and (len(self.deck)>0 or len(self.discard)>0):
-            self.draw()
         for i in self.buffs:
-            delete = i.activate()
-            if delete:
+            i.activate(0)
+            x = i.timeout()
+            if x: 
                 self.buffs.remove(i)
+        self.armour = 0
+        while self.handsize+self.temp>len(self.cards) and (len(self.deck)>0 or len(self.discard)>0) and len(self.cards)<9:
+            self.draw()
+        self.temp =0
+        for i in self.buffs:
+            i.activate([1])
         self.energy = self.baseenergy + extraenergy
 
 class card():
@@ -75,20 +90,38 @@ class card():
     
 
 class buff():
-    def __init__(self,name,duration,effect):
+    def __init__(self,name:str,duration:int,phases:list,aeffect,deffect,args):
         self.name = name
         self.duration = duration
-        self.effect = effect
+        self.phase = phases #[0] = before, [1] = during moves
+        self.aeffect = aeffect
+        self.deffect = deffect
+        self.args = args
 
-    def activate(self):
+    def activate(self,i):
+        if (i == 0 and self.phase[0] == 1) or (i ==1 and self.phase[1]== 1): 
+            self.aeffect(self.args)
+        
+
+    def timeout(self):
         self.duration -= 1
-        self.effect()
-        if self.duration <= 0:
+        if self.duration == 0:
+            self.deactivate()
             return 1
         return 0
 
+
+    def deactivate(self):
+        self.deffect(self.args)
+
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return self.__str__()
+
 class enemy():
-    def __init__(self,hp1:int,hp2:int,energy:int,moves:list):
+    def __init__(self,hp1:int,hp2:int,energy:int,moves:list,strength,buffs:list):
         self.hp1 = hp1
         self.hp2 = hp2
         self.energy = energy
@@ -98,6 +131,8 @@ class enemy():
         self.updateweight()
         self.prevhp1 = hp1
         self.prevhp2 = hp2
+        self.buffs = buffs
+        self.strength = strength
             
     def updateweight(self):
         self.weightsum = 0
@@ -109,7 +144,10 @@ class enemy():
 
     def play(self):
         randomizer = random.random() * self.weightsum
+        print(self.weightsum)
+        print(randomizer)
         index = bisect_left(self.weights, randomizer)
+        print(index)
         action = self.moves[index]
         print("baddie played",action.name)
         self.energy -= action.activate()
@@ -119,5 +157,5 @@ class enemy():
 
 class move(card):
     def __init__(self,name,cost,type,effect,args,description,weight):
-        super.__init__(name,cost,type,effect,args,description)
+        super().__init__(name,cost,type,effect,args,description)
         self.weight = weight #chance to be used
--- skele.py
+++ skele.py
@@ -3,66 +3,97 @@ neutral = 0
 lust = 1
 love = -1
 def main():
-
-    #cardlist
-    block = card("defend", 1, neutral, defend,4 , "Block 4 damage")
-    firt = card("help", 1,lust, lustdmg, 3, "Deal 3 Lust damage ")
-    help = card("flirt", 1,love, lovedmg, 2, "Deal 2 Love damage")
-    thoughtfulGift = card("Thoughtful gift",1, love, TG,(3,6), "Deal 3 Love Damage, if enemies Lust hp is higher, deal 6 Love damage") #(love dmg, if<dmg)
-    #visualAphrodisiac = card("Visual Aphrodisiac", 1, lust, (1,2), "Deal 1 Lust Damage, Lust damage is doubled for 2 turns)
-    scheduledMessage = card("Scheduled Message", 1, love,SM,3, "Shuffle one Surprise text into enemies draw pile, when drawn does 3 Love damage")
-    #gaslighting = card("Gaslighting", 0, love, GL,1,"Lose 1 max energy per turn. deal 4 love damage per turn" )
-    #selfLove = card("Self Love", 1, love, SL,1)
-    streak = card("Streak", 1, lust, ST, (1,2)) #tupple (dmg,carddraw)
-    #anklePic = card("Ankle Pic", 2, lust, AP,2)
-    #blockDM = card("Block DM", 3, lust, BDM,1)
-    wittyComeback = card("Witty Comeback", 1, neutral, WCB, (3,6)) #(lust dmg, love dmg)
-    #admittingFault = card("Admitting Fault", 0, neutral, AF, 2)
-    drunkText = card("Drunk Text", 1, neutral, DT,(3,6)) #(love dmg, lust dmg)
-
-    #movelist
-    move1 = move("pass",10,neutral,skip,1,1)
-    #coldShoulder = move("Cold Shoulder", 1,neutral,CS,3,1)
-    diggingUpThePast = move("Digging Up The Past", 1, neutral, DUTP,0,1)
-    #lastMinPlans = move("Last Minute Plans",2,neutral,LMP,6)
-    changeMyMind = move("Change My Mind",1, CMM,0,1)
-    #holdThatAgainstYou = move("Hold That Against You",1, neutral, HTAY,0,1)
-    k = move("k",3,neutral,K,12,1)
-
-    
     #to edit for balance
-    hp = 30
+    hp = 300
     armour = 0 
     hp1 = 20
     hp2 = 20
     energy = 3 
-    handsize = 4
+    handsize = 5
     buffs = []
     cards = []
     deck = []
     discard= []
-    moves = [move1]
+    boostvalue=[0,1,0,1]
+    strengthvalue = [0,1]
+    moves = []
     global you, baddie1
-    you = player(hp,armour,energy,handsize,cards,buffs,deck,discard)
-    baddie1 = enemy(hp1,hp2,energy,moves)
+    you = player(hp,armour,energy,handsize,cards,buffs,boostvalue,deck,discard)
+    baddie1 = enemy(hp1,hp2,energy,moves,strengthvalue,buffs)
+    
     turncounter = 0
     yourturn= 1
+
+    #bufflist
+    VAbuff = buff("aroused",2,[1,0],skip,boost,[0,2/3,0,1])
+    GLBuff = buff("gaslit",-1,[1,0],lovedmg,skip,4)
+    BDMbuff= buff("DM's blocked",1,[1,0],skip,bdm,0)
+    AFbuff = buff("Death sentenced",1,[1,0],skip,die,0)
+    CSbuff = buff("ignored",2,[1,0],skip,boost,[0,2,0,0.5])
+    LMPbuff = buff("busy",1,[1,0],skip,skip,1)
+    SLbuff = buff("Empowered",-1,[0,0],skip,skip,0)
+    Lbuff = buff("loved",2,[0,1],strength,skip,[1,1])
+    APbuff = buff("Smexy",-1,[0,0],skip,skip,0)
+    SYFbuff = buff("Embarassed",-1,[0,0],skip,skip,0)
+    HTAYbuff = buff("Pressured",-1,[0,0],skip,skip,0)
+
+    #cardlist
+    block = card("defend", 1, neutral, defend,4 , "Block 4 damage")
+    flirt = card("flirt", 1,lust, lustdmg, 3, "Deal 3 Lust damage ")
+    help = card("help", 1,love, lovedmg, 2, "Deal 2 Love damage")
+    thoughtfulGift = card("Thoughtful gift",1, love, TG,(3,6), "Deal 3 Love Damage, if enemies Lust hp is higher, deal 6 Love damage instead") #(love dmg, if<dmg)
+    visualAphrodisiac = card("Visual Aphrodisiac", 1, lust,VA, (1,VAbuff), "Deal 1 Lust Damage, Lust damage is doubled for 2 turns")
+    scheduledMessage = card("Scheduled Message", 1, love,SM,3, "Shuffle one Surprise text into enemies draw pile, when drawn does 3 Love damage")
+    gaslighting = card("Gaslighting", 2, love, GL,(1,GLBuff),"Lose 1 max energy per turn. deal 4 love damage per turn" )
+    selfLove = card("Self Love", 1, love, SL,(1,SLbuff), "Everytime you take damage draw a card")
+    streak = card("Streak", 1, lust, ST, (1,2),"Deal 1 Lust damage draw 2 cards")
+    anklePic = card("Ankle Pic", 2, lust, AP,APbuff,"Deal 2 more Lust damage for the rest of combat")
+    blockDM = card("Block DM", 3, lust, BDM,BDMbuff,"Become Invulnerable and unable to deal Love damage for one turn")
+    wittyComeback = card("Witty Comeback", 1, neutral, WCB, (3,6),"Deal 3 lust damage, if you took sanity damage last turn deal 6 love damage instead") #(lust dmg, love dmg)
+    admittingFault = card("Admitting Fault", 0, neutral, AF,(2,AFbuff),"Deal 2.5x damage. die next turn")
+    drunkText = card("Drunk Text", 1, neutral, DT,(3,6),"Deal 3 Love damage, if you are below half sanity, deal 6 lust damage instead") #(love dmg, lust dmg)
+    #movelist
+    attack = move("Attack",1,neutral,sanitydmg,4,"Deals 4 sanity damage",2)
+    coldShoulder = move("Cold Shoulder", 1,neutral,CS,(3,CSbuff),"Deal 3 Sanity damage, take halved lust damage and double love damage for one turn",1)
+    diggingUpThePast = move("Digging Up The Past", 1, neutral, DUTP,0,"Reflect Lust Damage back at player",1)
+    lastMinPlans = move("Last Minute Plans",2,neutral,LMP,(6,LMPbuff),"Deal 6 Sanity Damage, player draws one less card next turn",1)
+    changeMyMind = move("Change My Mind",1, neutral,CMM,0,"Swap Lust and Love HP",1)
+    holdThatAgainstYou = move("Hold That Against You",1, neutral, HTAY,HTAYbuff,"Every time you play a card take 1 unblockable damage",1)
+    showingFriendsText = move("Show Friends Your Text",2,neutral,SYF,([4,1],SYFbuff),"permernantly raises sanity damage by 4",1)
+    lovelanguage = move("Love Language",1, neutral,Love,Lbuff,"permernantly raises sanity damage by 1 for every love attack played during your next turn",1)
+    
+    k = move("k",3,neutral,K,12,"Deal 12 damage",1)
+
+    you.deck = [block,block,]
+    baddie1.moves = [holdThatAgainstYou]
+    baddie1.updateweight()
+    random.shuffle(you.deck)
+    you.newturn()
 
 
     print("Hello world")
     print("instructions and flavourtext go here")
     print()
     print()
+    
     while True:
         print("---------------------------------------")
         turncounter += 1
         print("turn",turncounter)
+        if baddie1.hp1 <=0 or baddie1.hp2 <= 0:
+            print("you win!")
+            return
+        if you.hp<=0:
+            print("you lose!")
+            return
         while yourturn:
             print()
             print()
-            print("Baddie   hp1:", baddie1.hp1, "     hp2:",baddie1.hp2)
+            print("Baddie   lust:", baddie1.hp1, "     love:",baddie1.hp2)
             print("You       hp:",you.hp," energy:",you.energy,"  armour:",you.armour )
             print("cards:", you.cards)
+            print("Buffs:",you.buffs)
+            print(baddie1.moves)
             done = False
             while not done:
                 x = input("what do? ")
@@ -83,7 +114,13 @@ def main():
         you.prevhp = you.hp
         baddie1.energy =3 - min(0,you.energy)
         while not yourturn:
-            
+            print()
+            print()
+            print("Baddie   lust:", baddie1.hp1, "     love:",baddie1.hp2)
+            print("You       hp:",you.hp," energy:",you.energy,"  armour:",you.armour )
+            print("cards:", you.cards)
+            print("Buffs:",you.buffs)
+            print(baddie1.moves)
             result = baddie1.play()
             if (result == 2):
                 yourturn = True
@@ -101,30 +138,38 @@ def defend(x):
     you.armour += x
 
 def lustdmg(x):
-    baddie1.hp1 -= x
+    baddie1.hp1 -= int((x + you.boost[0])*you.boost[1])
 
 def lovedmg(x):
-    baddie1.hp2 -= x
+    baddie1.hp2 -= int((x + you.boost[2])*you.boost[3]*you.bdm)
 
 def TG(x):
-    if baddie1.hp2<baddie1.hp1:
-        baddie1.hp2 -=x[0]
+    if baddie1.hp2<=baddie1.hp1:
+        lovedmg(x[0])
 
     else:
-        baddie1.hp2 -=x[1]
+        lovedmg(x[1])
 
 def SM(x):
-    SM = move("Scheduled Message", 0, SME(x), 1)
-    baddie1.moves.append(SM)
+    global SMmove
+    SMmove = move("Scheduled Message", 0,neutral, SME,(x),"bomb", 1)
+    baddie1.moves.append(SMmove)
+    baddie1.updateweight()
 
 def GL(x):
-    return
+    print(you.discard)
+    for i in you.discard:
+        if i.name == "Gaslighting":
+            you.discard.remove(i)
+    you.energy -=x[0]
+    you.buffs.append(x[1])
 
 def SL(x):
     for i in you.discard:
         if i.name == "Self Love":
             you.discard.remove(i)
-    return
+    you.selflove += x[0]
+    you.buffs.append(x[1])
 
 def ST(x):
     lustdmg(x[0])
@@ -132,7 +177,11 @@ def ST(x):
         you.draw()
 
 def AP(x):
-    return
+    for i in you.discard:
+        if i.name == "Ankle Pic":
+            you.discard.remove(i)
+    boost([2,1,0,1])
+    you.buffs.append(x)
 
 def WCB(x):
     if you.prevhp>you.hp:
@@ -140,45 +189,124 @@ def WCB(x):
     else:
         lustdmg(x[0])
 def AF(x):
-    return
+    boost([0,2.5,0,2.5])
+    you.buffs.append(x[1])
 
 def DT(x):
-    if you.maxhp/2 >= you.hp:
+    if you.maxhp/2 <= you.hp:
         lovedmg(x[0])
     else:
         lustdmg(x[1])
 
+def VA(x):
+    lustdmg(x[0])
+    for i in you.buffs:
+        if i.name == "aroused":
+            i.duration += 2
+            return
+    you.buffs.append(x[1])
+
+    boost([0,1.5,0,1])
+
+def BDM(x):
+    you.buffs.append(x)
+    you.bdm =0
 
 #move effects
-def skip():
-    pass
+
+
 def sanitydmg(x):
-    you.hp-=x
+    damage =  int((x + baddie1.strength[0])*baddie1.strength[1]*you.bdm)
+    if you.armour >= damage:
+        you.armour -= damage
+    else:
+        damage -= you.armour
+        you.armour =0
+        you.hp -= damage
+        if you.selflove > 0 :
+            you.temp += you.selflove
+
 def SME(x):
-    baddie1.hp2 -= x
-    baddie1.moves.remove(SM)
+    lovedmg(x)
+    baddie1.moves.remove(SMmove)
+    baddie1.updateweight()
 
 def CS(x):
-    sanitydmg(x)
+    sanitydmg(x[0])
+    for i in you.buffs:
+        if i.name == "ignored":
+            i.duration += 1
+            return
+    boost([0,0.5,0,2])
+    you.buffs.append(x[1])
+
+
+
 
 def DUTP(x):
     temp = baddie1.prevhp1 - baddie1.hp1
     sanitydmg(temp)
 
 def LMP(x):
-    sanitydmg(x)
+    sanitydmg(x[0])
+    you.temp-=1
+
 
 def CMM(x):
     baddie1.hp1,baddie1.hp2 = baddie1.hp2,baddie1.hp1
 
 def HTAY(x):
-    return
+    you.htay += 1
+    you.buffs.append(x)
 
 def K(x):
     sanitydmg(x)
 
+def Love(x):
+    for i in you.buffs:
+        if i.name == "loved":
+            i.duration += 1
+            return
+    you.buffs.append(x)
+
+def SYF(x):
+    strength(x[0])
+    you.buffs.append(x[1])
 
 
+
+#buffs aeffects/deffects
+
+def boost(x):
+    you.boost[0] += x[0]
+    you.boost[1] *= x[1]
+    you.boost[2] += x[2]
+    you.boost[3] *= x[3]
+
+def strength(x):
+    baddie1.strength[0] += x[0]
+    baddie1.strength[1] *= x[1]
+
+def die(*args):
+    you.hp = -99999
+
+def carddraw(x):
+    for i in range(x):
+        you.draw()
+
+def energy(x):
+    x[0].energy += x[1]
+
+def discarddmg(x):
+    you.hp -= x * len(you.discard)
+
+def bdm(*x):
+    you.bdm=1 
+
+
+# helper effects
+def skip(*args):
+    pass
 
 if __name__ == "__main__":
     main()
